e45ebb026031dcc23a60d1de1917e8c2b2071a02
Luan Eli Oliveira <luannn@gmail.com>
2019-08-12 23:26:23 -0300

    Adicionar trativa dos erros do erro não encontrado e ID inválido

--- backend/src/main/java/br/com/codenation/centralerrosapi/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,4 +1,27 @@
 package br.com.codenation.centralerrosapi.controllers.exceptions;
 
+import br.com.codenation.centralerrosapi.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest r) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), r.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<StandardError> illegalArgument(IllegalArgumentException e, HttpServletRequest r){
+
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), r.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
 }
--- backend/src/main/java/br/com/codenation/centralerrosapi/controllers/exceptions/StandardError.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/controllers/exceptions/StandardError.java
@@ -1,4 +1,23 @@
 package br.com.codenation.centralerrosapi.controllers.exceptions;
 
-public class StandardError {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String messsage;
+    private String path;
+
 }
