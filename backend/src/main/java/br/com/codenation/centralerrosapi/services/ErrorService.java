bfd912560d5c4a8dcb7f8f8eb789d95be8c1771c
Luan Eli Oliveira <luannn@gmail.com>
2019-08-12 23:24:44 -0300

    Adicionar a busca do erro por id

--- backend/src/main/java/br/com/codenation/centralerrosapi/services/ErrorService.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/services/ErrorService.java
@@ -2,10 +2,12 @@ package br.com.codenation.centralerrosapi.services;
 
 import br.com.codenation.centralerrosapi.models.Error;
 import br.com.codenation.centralerrosapi.repositories.ErrorRepository;
+import br.com.codenation.centralerrosapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ErrorService {
@@ -17,4 +19,7 @@ public class ErrorService {
         return repository.findAll();
     }
 
+    public Error findById(UUID id) {
+        return repository.findById(id).orElseThrow(ObjectNotFoundException::new);
+    }
 }
--- backend/src/main/java/br/com/codenation/centralerrosapi/services/exceptions/ObjectNotFoundException.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/services/exceptions/ObjectNotFoundException.java
@@ -1,4 +1,18 @@
 package br.com.codenation.centralerrosapi.services.exceptions;
 
-public class ObjectNotFoundException {
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException() {
+        super("Not found");
+    }
 }
