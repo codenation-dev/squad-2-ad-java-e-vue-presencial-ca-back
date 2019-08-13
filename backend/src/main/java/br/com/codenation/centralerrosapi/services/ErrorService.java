ed8252e9165877a32ac44c396e828f44b38b4961
Luan Eli Oliveira <luannn@gmail.com>
2019-08-12 23:49:45 -0300

    Alterar o objeto da excessão quando não encontrar o registro

--- backend/src/main/java/br/com/codenation/centralerrosapi/services/ErrorService.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/services/ErrorService.java
@@ -2,10 +2,10 @@ package br.com.codenation.centralerrosapi.services;
 
 import br.com.codenation.centralerrosapi.models.Error;
 import br.com.codenation.centralerrosapi.repositories.ErrorRepository;
-import br.com.codenation.centralerrosapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +20,6 @@ public class ErrorService {
     }
 
     public Error findById(UUID id) {
-        return repository.findById(id).orElseThrow(ObjectNotFoundException::new);
+        return repository.findById(id).orElseThrow(() -> new NoResultException("Error not found with the specified id"));
     }
 }
