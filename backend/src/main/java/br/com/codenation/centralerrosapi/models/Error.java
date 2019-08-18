5cf1c32fc506329b932f418891fcef71e53b1a55
Luan Eli Oliveira <luannn@gmail.com>
2019-08-18 11:53:04 -0300

    Alterar o strategy do identificador da classe de erros

--- backend/src/main/java/br/com/codenation/centralerrosapi/models/Error.java
+++ backend/src/main/java/br/com/codenation/centralerrosapi/models/Error.java
@@ -19,7 +19,7 @@ public class Error implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
     private String title;
     private String detail;
