a6a4362c4a671b7c617e49eb44220900d60e0f00
Luis Fernando Carvalho <luiscvlh11@gmail.com>
2019-09-04 19:44:08 -0300

    Initial commit by front

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Container>
+      <FormLogin></FormLogin>
+    </Container>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import Container from "@/components/Container";
+import FormLogin from "@/views/Login/Form";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { Navbar, Container, FormLogin }
+};
+</script>
---
+++ frontend/src/views/Login/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      <h1 class="text-center">Login</h1>
+      <b-form>
+        <b-form-group label="Email" description="Ex.: exemplo@exemplo.com">
+          <b-form-input
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Digite seu e-mail de acesso"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Password">
+          <b-form-input v-model="form.password" type="password" required></b-form-input>
+        </b-form-group>
+        <b-button class="text-center" type="submit" variant="primary">Login</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
