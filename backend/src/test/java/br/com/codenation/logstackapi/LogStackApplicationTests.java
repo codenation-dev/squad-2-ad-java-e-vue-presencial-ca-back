9be85cafdeb9ec65c6af3eb20ee3986ac08f3c32
Luan Eli Oliveira <luannn@gmail.com>
2019-09-08 20:47:37 -0300

    Adicionar testes unitários das classes de entidades

--- backend/src/test/java/br/com/codenation/logstackapi/LogStackApplicationTests.java
+++ backend/src/test/java/br/com/codenation/logstackapi/LogStackApplicationTests.java
@@ -1,12 +1,18 @@
 package br.com.codenation.logstackapi;
 
 import br.com.codenation.logstackapi.dto.UserCreateDTOTest;
+import br.com.codenation.logstackapi.model.entity.LogApplicationTest;
+import br.com.codenation.logstackapi.model.entity.LogDetailTest;
+import br.com.codenation.logstackapi.model.entity.LogTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-		UserCreateDTOTest.class
+		UserCreateDTOTest.class,
+		LogTest.class,
+		LogDetailTest.class,
+		LogApplicationTest.class
 })
 public class LogStackApplicationTests {
 
---
+++ backend/src/test/java/br/com/codenation/logstackapi/builders/LogBuilder.java
@@ -0,0 +1,76 @@
+package br.com.codenation.logstackapi.builders;
+
+import br.com.codenation.logstackapi.model.entity.Log;
+import br.com.codenation.logstackapi.model.entity.LogApplication;
+import br.com.codenation.logstackapi.model.entity.LogDetail;
+
+import java.util.UUID;
+
+public class LogBuilder {
+
+    private Log log;
+
+    private LogBuilder() {
+    }
+
+    public static LogBuilder umLog() {
+        LogBuilder builder = new LogBuilder();
+        builder.log = Log.builder()
+                .id(UUID.randomUUID())
+                .title("Título")
+                .archived(false)
+                .build();
+        return builder;
+    }
+
+    public LogBuilder arquivado() {
+        log.setArchived(true);
+        return this;
+    }
+
+    public LogBuilder naoArquivado() {
+        log.setArchived(false);
+        return this;
+    }
+
+    public LogBuilder comLevelError() {
+        LogDetail detail = LogDetailBuilder.detalhe().comLevelError().build();
+        log.setDetail(detail);
+        return this;
+    }
+
+    public LogBuilder comLevelDebug() {
+        LogDetail detail = LogDetailBuilder.detalhe().comLevelDebug().build();
+        log.setDetail(detail);
+        return this;
+    }
+
+    public LogBuilder comLevelWarning() {
+        LogDetail detail = LogDetailBuilder.detalhe().comLevelWarning().build();
+        log.setDetail(detail);
+        return this;
+    }
+
+    public LogBuilder emDesenvolvimento() {
+        LogApplication application = LogApplicationBuilder.umaAplicacao().emDesenvolvimento().build();
+        log.setApplication(application);
+        return this;
+    }
+
+    public LogBuilder emTeste() {
+        LogApplication application = LogApplicationBuilder.umaAplicacao().emTeste().build();
+        log.setApplication(application);
+        return this;
+    }
+
+    public LogBuilder emProducao() {
+        LogApplication application = LogApplicationBuilder.umaAplicacao().emProducao().build();
+        log.setApplication(application);
+        return this;
+    }
+
+    public Log build() {
+        return log;
+    }
+
+}
