bde93a0bc12c7df24ef72a92b609a3289a1b7d7f
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-10 15:57:30 -0300

    Adicionar documentação da API nos DTOs

--- src/main/java/br/com/codenation/logstackapi/dto/UserDTO.java
+++ src/main/java/br/com/codenation/logstackapi/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package br.com.codenation.logstackapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,10 +9,16 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "fullName", "email"})
 public class UserDTO {
 
+    @ApiModelProperty(value = "Identificador do usuário", position = 1, example = "cbd9881e-88e9-4973-bfc0-5b4fcde29574")
     private UUID id;
+
+    @ApiModelProperty(value = "Nome do usuário", position = 2, example = "Administrador do Sistema")
     private String fullName;
+
+    @ApiModelProperty(value = "Email do usuário", position = 3, example = "admin@example.com")
     private String email;
 
 }
