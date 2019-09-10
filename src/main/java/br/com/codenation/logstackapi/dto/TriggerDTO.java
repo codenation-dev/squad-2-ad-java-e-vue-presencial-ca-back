2aa019c96e990bf235551faf8c5f5e3eec8b575e
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-10 14:10:19 -0300

    Adicionar o mapeamento DTO/Entidade do filtro

--- src/main/java/br/com/codenation/logstackapi/dto/TriggerDTO.java
+++ src/main/java/br/com/codenation/logstackapi/dto/TriggerDTO.java
@@ -1,20 +1,28 @@
 package br.com.codenation.logstackapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "name", "filters", "isActive"})
 public class TriggerDTO {
 
+    @ApiModelProperty(value = "Identificador do gatilho", position = 1, example = "cbd9881e-88e9-4973-bfc0-5b4fcde29574")
     private UUID id;
+
+    @ApiModelProperty(value = "Nome do gatilho", position = 2, example = "Level Erro em produção da API LogStack")
     private String name;
-    private String appName;
-    private String environment;
-    private String level;
-    private UserDTO createdBy;
-    private UserDTO lastModifiedBy;
+
+    @ApiModelProperty(value = "Filtros do gatilho", position = 3)
+    private List<TriggerFilterDTO> filters;
+
+    @ApiModelProperty(value = "Status do gatilho", position = 4)
+    private Boolean isActive;
 
 }
