4de505cba12721f87caba0b885b5c6ce24b96fce
Luan Eli Oliveira <luannn@gmail.com>
2019-09-12 20:21:11 -0300

    Corrigir teste unitário da criação do gatilho

--- src/test/java/br/com/codenation/logstackapi/dto/TriggerCreateDTOTest.java
+++ src/test/java/br/com/codenation/logstackapi/dto/TriggerCreateDTOTest.java
@@ -1,9 +1,7 @@
 package br.com.codenation.logstackapi.dto;
 
-import br.com.codenation.logstackapi.model.entity.LogApplication;
 import br.com.codenation.logstackapi.model.enums.LogEnvironment;
 import br.com.codenation.logstackapi.model.enums.LogLevel;
-import br.com.codenation.logstackapi.model.enums.TriggerFilterField;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,18 +10,19 @@ public class TriggerCreateDTOTest {
 
     @Test
     public void deveRetornarDTONulo() {
-        TriggerCreateDTO dto = new TriggerCreateDTO();
+        TriggerCreateDTO dto  = new TriggerCreateDTO();
         assertThat(dto.isNull()).isTrue();
     }
 
     @Test
     public void deveRetornarDTOPreenchido() {
-        TriggerCreateDTO dto = new TriggerCreateDTO();
-        TriggerFilterDTO filter = TriggerFilterDTO.builder()
+        TriggerFilterCreateDTO filter = TriggerFilterCreateDTO.builder()
                 .appName("logstack-api")
                 .level(LogLevel.ERROR)
                 .environment(LogEnvironment.PRODUCTION)
                 .build();
+        TriggerCreateDTO dto = new TriggerCreateDTO();
+        dto.setFilters(filter);
         assertThat(dto.isNull()).isFalse();
     }
 }
