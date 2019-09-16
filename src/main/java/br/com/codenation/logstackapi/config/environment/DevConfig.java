45634aefa29b9913940d74c921ae7d4697f6b5e6
Luan Eli Oliveira <luannn@gmail.com>
2019-09-16 19:21:33 -0300

    Criar arquivo de configuração do ambiente de dev

---
+++ src/main/java/br/com/codenation/logstackapi/config/environment/DevConfig.java
@@ -0,0 +1,31 @@
+package br.com.codenation.logstackapi.config.environment;
+
+import br.com.codenation.logstackapi.service.impl.DBServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    DBServiceImpl dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)) return false;
+
+        dbService.instantiateTestDatabase();
+
+        return true;
+    }
+}
