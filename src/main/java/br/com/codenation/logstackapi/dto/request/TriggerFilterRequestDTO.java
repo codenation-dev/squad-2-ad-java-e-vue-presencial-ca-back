2cb54c26f77445bbf8794e819f1c5cfbf4ad6249
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-16 16:23:19 -0300

    Incluir verificação do atributos nulos

--- src/main/java/br/com/codenation/logstackapi/dto/request/TriggerFilterRequestDTO.java
+++ src/main/java/br/com/codenation/logstackapi/dto/request/TriggerFilterRequestDTO.java
@@ -2,6 +2,7 @@ package br.com.codenation.logstackapi.dto.request;
 
 import br.com.codenation.logstackapi.model.enums.LogEnvironment;
 import br.com.codenation.logstackapi.model.enums.LogLevel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Data
 @NoArgsConstructor
@@ -31,5 +34,10 @@ public class TriggerFilterRequestDTO {
     @ApiModelProperty(value = "Nível do log", position = 3, example = "ERROR", allowableValues = "DEBUG, LEVEL, ENVIRONMENT", required = true)
     @NotNull
     private LogLevel level;
+
+    @JsonIgnore
+    public boolean isNull() {
+        return Stream.of(appName, environment, level).allMatch(Objects::isNull);
+    }
 
 }
