d55b86da86d05e0f5870d35893a977f93b23e821
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-16 16:21:24 -0300

    Incluir obrigatoriedade dos campos da requisição do gatilho

--- src/main/java/br/com/codenation/logstackapi/dto/request/TriggerFilterRequestDTO.java
+++ src/main/java/br/com/codenation/logstackapi/dto/request/TriggerFilterRequestDTO.java
@@ -9,6 +9,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,13 +19,17 @@ import lombok.NoArgsConstructor;
 @JsonPropertyOrder({"appName", "environment", "level"})
 public class TriggerFilterRequestDTO {
 
-    @ApiModelProperty(value = "Nome da aplicação", position = 1, example = "logstack-api")
+    @ApiModelProperty(value = "Nome da aplicação", position = 1, example = "logstack-api", required = true)
+    @Size(min = 1, max = 255)
+    @NotNull
     private String appName;
 
     @ApiModelProperty(value = "Ambiente da aplicação", position = 2, example = "PRODUCTION", allowableValues = "DEVELOPMENT, TEST, PRODUCTION", required = true)
+    @NotNull
     private LogEnvironment environment;
 
     @ApiModelProperty(value = "Nível do log", position = 3, example = "ERROR", allowableValues = "DEBUG, LEVEL, ENVIRONMENT", required = true)
+    @NotNull
     private LogLevel level;
 
 }
