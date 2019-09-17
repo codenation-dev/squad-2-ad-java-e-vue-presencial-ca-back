bc5f73a277859ade5c4dfabbd073e33bcde52b56
Luan Eli Oliveira <luannn@gmail.com>
2019-09-16 23:35:57 -0300

    Adicionar testes unitários na LogController para validar o retorno da lista de logs

--- src/test/java/br/com/codenation/logstackapi/LogStackApplicationTests.java
+++ src/test/java/br/com/codenation/logstackapi/LogStackApplicationTests.java
@@ -3,10 +3,8 @@ package br.com.codenation.logstackapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LogStackApplicationTests {
--- src/test/java/br/com/codenation/logstackapi/controller/LogControllerTest.java
+++ src/test/java/br/com/codenation/logstackapi/controller/LogControllerTest.java
@@ -6,13 +6,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -33,6 +38,15 @@ public class LogControllerTest {
 
         mvc.perform(MockMvcRequestBuilders.get(URI + id.toString()))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void dadoNenhumLogExistente_quandoBuscarTodosLogs_deveRetornarNenhumLog() throws Exception {
+        mvc.perform(get(URI)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(0)));
     }
 
 }
