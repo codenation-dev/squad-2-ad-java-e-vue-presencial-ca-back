089824ff0540de992ddae422dcf89878714bcfc5
Luan Eli Oliveira <luannn@gmail.com>
2019-09-21 19:37:04 -0300

    Adicionar entidade da mensagem de email

---
+++ src/main/java/br/com/codenation/logstackapi/model/entity/EmailMessage.java
@@ -0,0 +1,18 @@
+package br.com.codenation.logstackapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailMessage {
+
+    private String to;
+    private String subject;
+    private String body;
+
+}
