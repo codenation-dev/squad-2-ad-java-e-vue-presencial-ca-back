ae026cd9ffaa286e253124836ef2afe1a914c41f
Luan Eli Oliveira <luannn@gmail.com>
2019-09-21 23:53:11 -0300

    Alterar a interface de notificação do alerta

--- src/main/java/br/com/codenation/logstackapi/service/NotificationsService.java
+++
@@ -1,9 +0,0 @@
-package br.com.codenation.logstackapi.service;
-
-import br.com.codenation.logstackapi.model.entity.EmailMessage;
-
-public interface NotificationsService {
-
-    void sendEmail(EmailMessage emailMessage);
-
-}
