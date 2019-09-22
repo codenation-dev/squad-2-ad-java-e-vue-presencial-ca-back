a2bfa7fe3c0884689248a20b6e880f55d60137b9
Luan Eli Oliveira <luannn@gmail.com>
2019-09-22 02:09:15 -0300

    Adicionar a implementação da autenticaçao de usuários

--- src/test/java/br/com/codenation/logstackapi/controller/LogControllerTest.java
+++ src/test/java/br/com/codenation/logstackapi/controller/LogControllerTest.java
@@ -1,22 +1,31 @@
 package br.com.codenation.logstackapi.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 public class LogControllerTest {
 
     private static String URI = "/api/v1/logs";
@@ -31,19 +41,37 @@ public class LogControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Value("${security.oauth2.client.client-id}")
+    private String client;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String secret;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private JacksonJsonParser parser = new JacksonJsonParser();
+    private String token = "";
+
+    @Before
+    public void beforeTests() throws Exception {
+        token = generateToken();
+    }
+
     @Test
     public void dadoLogInexistente_quandoBuscoPorId_entaoRetornaNaoEncontrado() throws Exception {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.get(URI + id.toString()))
+        mvc.perform(MockMvcRequestBuilders.get(URI + "/" + id.toString())
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     @Transactional
     public void dadoNenhumLogExistente_quandoBuscarTodosLogs_deveRetornarNenhumLog() throws Exception {
+
         mvc.perform(get(URI)
+                .header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(0)));
@@ -54,7 +82,8 @@ public class LogControllerTest {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.put(URI + id.toString() + "/archive"))
+        mvc.perform(MockMvcRequestBuilders.put(URI + "/" + id.toString() + "/archive")
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
@@ -63,8 +92,31 @@ public class LogControllerTest {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.delete(URI + id.toString() + "/archive"))
+        mvc.perform(MockMvcRequestBuilders.delete(URI + "/" + id.toString() + "/archive")
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
+    private String generateToken() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", "admin@example.com");
+        params.add("password", "admin");
+
+        ResultActions login = mvc.perform(
+                post("/oauth/token")
+                        .params(params)
+                        .accept("application/json;charset=UTF-8")
+                        .with(httpBasic(client, secret)))
+                .andExpect(status().isOk());
+
+        String token = parser.parseMap(login
+                .andReturn()
+                .getResponse()
+                .getContentAsString()).get("access_token").toString();
+
+        return String.format("Bearer %s", token);
+
+    }
 }
--- src/test/java/br/com/codenation/logstackapi/controller/TriggerControllerTest.java
+++ src/test/java/br/com/codenation/logstackapi/controller/TriggerControllerTest.java
@@ -1,13 +1,16 @@
 package br.com.codenation.logstackapi.controller;
 
 import br.com.codenation.logstackapi.builders.TriggerBuilder;
-import br.com.codenation.logstackapi.dto.request.TriggerRequestDTO;
 import br.com.codenation.logstackapi.model.entity.Trigger;
 import br.com.codenation.logstackapi.repository.TriggerRepository;
 import br.com.codenation.logstackapi.service.TriggerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -17,13 +20,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.UUID;
 
-import static br.com.codenation.logstackapi.TestUtil.convertObjectToJsonBytes;
+import static br.com.codenation.logstackapi.util.TestUtil.convertObjectToJsonBytes;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,12 +51,28 @@ public class TriggerControllerTest {
     @Autowired
     private TriggerRepository repository;
 
+    @Value("${security.oauth2.client.client-id}")
+    private String client;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String secret;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private JacksonJsonParser parser = new JacksonJsonParser();
+    private String token = "";
+
+    @Before
+    public void beforeTests() throws Exception {
+        token = generateToken();
+    }
+
     @Test
     public void dadoTriggerInexistente_quandoBuscoPorId_entaoRetornaNaoEncontrado() throws Exception {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.get(URI + id.toString()))
+        mvc.perform(MockMvcRequestBuilders.get(URI + id.toString())
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
@@ -58,6 +81,7 @@ public class TriggerControllerTest {
     public void dadoNenhumaTriggerExistente_quandoBuscarTodasTrigger_deveRetornarNenhumaTrigger() throws Exception {
 
         mvc.perform(get(URI)
+                .header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
@@ -68,7 +92,8 @@ public class TriggerControllerTest {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.put(URI + id.toString() + "/archive"))
+        mvc.perform(MockMvcRequestBuilders.put(URI + id.toString() + "/archive")
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
@@ -77,7 +102,8 @@ public class TriggerControllerTest {
 
         UUID id = UUID.randomUUID();
 
-        mvc.perform(MockMvcRequestBuilders.delete(URI + id.toString() + "/archive"))
+        mvc.perform(MockMvcRequestBuilders.delete(URI + id.toString() + "/archive")
+                .header("Authorization", token))
                 .andExpect(status().isNotFound());
     }
 
@@ -89,6 +115,7 @@ public class TriggerControllerTest {
         this.repository.save(trigger);
 
         ResultActions perform = mvc.perform(get(URI)
+                .header("Authorization", token)
                 .content(convertObjectToJsonBytes(trigger))
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(status().isOk())
@@ -96,5 +123,28 @@ public class TriggerControllerTest {
 
         perform.andExpect(jsonPath("$[0].name", is("Trigger 4 de demonstração")));
         perform.andExpect(jsonPath("$[0].message", is("Message 4")));
+    }
+
+    private String generateToken() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", "admin@example.com");
+        params.add("password", "admin");
+
+        ResultActions login = mvc.perform(
+                post("/oauth/token")
+                        .params(params)
+                        .accept("application/json;charset=UTF-8")
+                        .with(httpBasic(client, secret)))
+                .andExpect(status().isOk());
+
+        String token = parser.parseMap(login
+                .andReturn()
+                .getResponse()
+                .getContentAsString()).get("access_token").toString();
+
+        return String.format("Bearer %s", token);
+
     }
 }
