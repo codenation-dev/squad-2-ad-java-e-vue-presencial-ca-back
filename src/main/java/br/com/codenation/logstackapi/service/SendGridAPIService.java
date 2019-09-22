2c45fb7eb29cf15dd4d3f273eecd8e366b6e3f6e
Luan Eli Oliveira <luannn@gmail.com>
2019-09-21 23:52:06 -0300

    Excluir interface que não será utilizada

--- src/main/java/br/com/codenation/logstackapi/service/SendGridAPIService.java
+++
@@ -1,8 +0,0 @@
-package br.com.codenation.logstackapi.service;
-
-import com.sendgrid.Response;
-
-public interface SendGridAPIService {
-
-    Response sendEmail(String to, String subject, String body, String type);
-}
