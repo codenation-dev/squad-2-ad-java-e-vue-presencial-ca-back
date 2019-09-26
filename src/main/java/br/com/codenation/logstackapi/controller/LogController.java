b2030045dcf096c647a535c84349a52d8153f838
Luan Eli Oliveira <luannn@gmail.com>
2019-09-26 00:06:20 -0300

    Adicionar Endpoint para exportação de logs em CSV

--- src/main/java/br/com/codenation/logstackapi/controller/LogController.java
+++ src/main/java/br/com/codenation/logstackapi/controller/LogController.java
@@ -5,10 +5,16 @@ import br.com.codenation.logstackapi.dto.request.LogSearchDTO;
 import br.com.codenation.logstackapi.dto.response.ErrorResponseDTO;
 import br.com.codenation.logstackapi.dto.response.LogDetailResponseDTO;
 import br.com.codenation.logstackapi.dto.response.LogResponseDTO;
+import br.com.codenation.logstackapi.exception.ApiError;
 import br.com.codenation.logstackapi.mappers.LogMapper;
 import br.com.codenation.logstackapi.model.enums.LogEnvironment;
 import br.com.codenation.logstackapi.model.enums.LogLevel;
 import br.com.codenation.logstackapi.service.impl.LogServiceImpl;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -17,13 +23,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -37,18 +47,75 @@ public class LogController {
     private LogMapper mapper;
 
     @ApiOperation(
+            value = "Exporta os logs pesquisados por um filtro",
+            notes = "Método utilizado para exportar os logs pesquisados por um filtro."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Requisição mal formatada", response = ApiError.class),
+            @ApiResponse(code = 500, message = "Erro na api", response = ApiError.class)
+    })
+    @GetMapping(value = "/logs/export", produces = "text/csv")
+    private void exportCSV(
+            HttpServletResponse response,
+            @RequestParam(value = "title", required = false) Optional<String> title,
+            @RequestParam(value = "appName", required = false) Optional<String> appName,
+            @RequestParam(value = "host", required = false) Optional<String> host,
+            @RequestParam(value = "ip", required = false) Optional<String> ip,
+            @RequestParam(value = "environment", required = false) Optional<LogEnvironment> environment,
+            @RequestParam(value = "level", required = false) Optional<LogLevel> level,
+            @RequestParam(value = "startTimestamp", required = false, defaultValue = "2019-09-01")
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startTimestamp,
+            @RequestParam(value = "endTimestamp", required = false, defaultValue = "2019-09-30")
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endTimestamp,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "detail.timestamp");
+
+        LogSearchDTO search = LogSearchDTO.builder()
+                .title(title.map(String::toLowerCase).orElse(null))
+                .appName(appName.map(String::toLowerCase).orElse(null))
+                .host(host.map(String::toLowerCase).orElse(null))
+                .ip(ip.map(String::toLowerCase).orElse(null))
+                .environment(environment.orElse(null))
+                .level(level.orElse(null))
+                .startTimestamp(LocalDateTime.of(startTimestamp, LocalTime.of(0, 0, 0)))
+                .endTimestamp(LocalDateTime.of(endTimestamp, LocalTime.of(23, 59, 59)))
+                .build();
+
+        List<LogResponseDTO> logs = service.find(search, page, size, sort).map(mapper::map).getContent();
+
+        String filename = "logs.csv";
+
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"");
+
+        StatefulBeanToCsv<LogResponseDTO> writer = new StatefulBeanToCsvBuilder<LogResponseDTO>(response.getWriter())
+                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .withOrderedResults(false)
+                .build();
+
+        //write all users to csv file
+        writer.write(logs);
+
+    }
+
+    @ApiOperation(
             value = "Recupera todos os logs cadastrados",
             notes = "Método utilizado para recuperar todos os logs cadastrados."
     )
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "OK", response = LogResponseDTO.class, responseContainer = "List"),
-            @ApiResponse(code = 400, message = "Requisição mal formatada", response = ErrorResponseDTO.class),
-            @ApiResponse(code = 500, message = "Erro na api", response = ErrorResponseDTO.class)
+            @ApiResponse(code = 400, message = "Requisição mal formatada", response = Api.class),
+            @ApiResponse(code = 500, message = "Erro na api", response = ApiError.class)
     })
     @GetMapping(value = "/logs", produces = MediaType.APPLICATION_JSON_VALUE)
     private Page<LogResponseDTO> find(
             @RequestParam(value = "title", required = false) Optional<String> title,
-            @RequestParam(value = "app_name", required = false) Optional<String> appName,
+            @RequestParam(value = "appName", required = false) Optional<String> appName,
             @RequestParam(value = "host", required = false) Optional<String> host,
             @RequestParam(value = "ip", required = false) Optional<String> ip,
             @RequestParam(value = "environment", required = false) Optional<LogEnvironment> environment,
@@ -75,6 +142,7 @@ public class LogController {
 
         return service.find(search, page, size, sort).map(mapper::map);
     }
+
 
     @ApiOperation(
             value = "Recupera um log específico.",
