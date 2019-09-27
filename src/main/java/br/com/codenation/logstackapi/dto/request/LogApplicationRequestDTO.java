e67850184a5585606eb852a28dedad8b94f357e0
Luan Eli Oliveira <luannn@gmail.com>
2019-09-27 07:42:09 -0300

    Ajustar alinhamento do código

--- src/main/java/br/com/codenation/logstackapi/dto/request/LogApplicationRequestDTO.java
+++ src/main/java/br/com/codenation/logstackapi/dto/request/LogApplicationRequestDTO.java
@@ -17,40 +17,19 @@ import javax.validation.constraints.NotNull;
 @JsonPropertyOrder({"name", "host", "ip", "environment"})
 public class LogApplicationRequestDTO {
 
-    @ApiModelProperty(
-            value = "Nome da aplicação",
-            position = 1,
-            example = "logstack-api",
-            required = true
-    )
+    @ApiModelProperty(value = "Nome da aplicação", position = 1, example = "logstack-api", required = true)
     @NotNull
     private String name;
 
-    @ApiModelProperty(
-            value = "Hostname da aplicação",
-            position = 2,
-            example = "logstack-api.herokuapp.com",
-            required = true
-    )
+    @ApiModelProperty(value = "Hostname da aplicação", position = 2, example = "logstack-api.herokuapp.com", required = true)
     @NotNull
     private String host;
 
-    @ApiModelProperty(
-            value = "Ip da aplicação",
-            position = 3,
-            example = "184.456.41.11",
-            required = true
-    )
+    @ApiModelProperty(value = "Ip da aplicação", position = 3, example = "184.456.41.11", required = true)
     @NotNull
     private String ip;
 
-    @ApiModelProperty(
-            value = "Ambiente da aplicação",
-            position = 4,
-            example = "PRODUCTION",
-            allowableValues = "DEVELOPMENT, TEST, PRODUCTION",
-            required = true
-    )
+    @ApiModelProperty(value = "Ambiente da aplicação", position = 4, example = "PRODUCTION", allowableValues = "DEVELOPMENT, TEST, PRODUCTION", required = true)
     @NotNull
     private LogEnvironment environment;
 
